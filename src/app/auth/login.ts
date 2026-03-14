a67a7a39624ac351d726026051cc47510f2d439d
paul-ventura-git <j.paul.ventura@gmail.com>
2026-03-13 21:45:01 -0500

    127

--- src/app/auth/login.ts
+++ src/app/auth/login.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, ElementRef, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,40 @@ import { AuthService } from './auth.service';
   styleUrls: ['./login.css'],
   imports: [ReactiveFormsModule],
 })
-export default class Login {
-  _formBuilder = inject(FormBuilder);
+export default class Login implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @ViewChild('myPopover') input!: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      import('bootstrap').then((bootstrap) => {
+
+        const pop = new bootstrap.Popover(this.input.nativeElement, {
+          trigger: 'focus',
+          placement: 'top',
+          html: true,
+          title: 'Intente ingresando como:',
+          content: `
+<div>
+  <div><b>admin</b>: admin@miempresa.com</div>
+  <div><b>cashier</b>: cashier@miempresa.com</div>
+  <div><b>customer</b>: customer@miempresa.com</div>
+  <div><b>guest</b>: visitor@miempresa.com</div>
+  <hr style="margin:6px 0">
+  <div><b>Password:</b> any</div>
+</div>
+          `
+        });
+
+        pop.show();
+
+      });
+    }
+  }
+
+  _formBuilder = inject(FormBuilder);
   _authService = inject(AuthService);
 
   form = this._formBuilder.group({
@@ -20,9 +52,7 @@ export default class Login {
 
   login() {
     if (!this.form.valid) return;
-
     const email = this.form.value!.email!;
-
     this._authService.login(email).subscribe();
   }
 }
